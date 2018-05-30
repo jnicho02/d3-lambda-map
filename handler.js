ce77854ffa896185e6779b9daac2324b1b6dad9f
Jez Nicholson <jez.nicholson@gmail.com>
2018-05-30 13:28:57 +0100

    does some svg
    needs to zoom in

--- handler.js
+++ handler.js
@@ -1,16 +1,41 @@
-'use strict';
+'use strict'
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+const D3Node = require('d3-node')
+const d3 = require('d3');
+const canvasModule = require('canvas');
+const topojson = require('topojson');
+
+module.exports.plot = (event, context, callback) => {
+  const us = require('./us-states.json');
+//  const D3Node = require('./../index');
+
+  // adapted from: https://bl.ocks.org/mbostock/6406992
+  var options = {
+    svgStyles:'.mesh{fill: none;stroke: #333;stroke-width: .5px;stroke-linejoin: round;}',
+    d3Module: d3
   };
 
-  callback(null, response);
+  var d3n = new D3Node(options);
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+  var width = 960,
+    height = 500;
+
+  var path = d3.geoPath()
+    .projection(null);
+
+  var svg = d3n.createSVG(width, height)
+    .append('path')
+    .datum(topojson.mesh(us))
+    .attr('class', 'mesh')
+    .attr('d', path);
+
+  const response = {
+    statusCode: 200,
+    headers: {
+     "Content-Type" : "image/svg+xml"
+    },
+    body: d3n.svgString(),
+  }
+
+  callback(null, response)
+}
