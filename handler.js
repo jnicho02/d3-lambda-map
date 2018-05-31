357ca1f01858f88dd41a9f5d48358c7c92af58ed
Jez Nicholson <jez.nicholson@gmail.com>
2018-05-31 08:46:14 +0100

    still tiny

--- handler.js
+++ handler.js
@@ -1,40 +1,54 @@
 'use strict'
 
 const D3Node = require('d3-node')
-const d3 = require('d3');
-const canvasModule = require('canvas');
-const topojson = require('topojson');
+const d3 = require('d3')
+const topojson = require('topojson')
 
 module.exports.plot = (event, context, callback) => {
-  const us = require('./us-states.json');
-//  const D3Node = require('./../index');
+  const us = require('./us-states.json')
 
   // adapted from: https://bl.ocks.org/mbostock/6406992
   var options = {
     svgStyles:'.mesh{fill: none;stroke: #333;stroke-width: .5px;stroke-linejoin: round;}',
     d3Module: d3
-  };
-
-  var d3n = new D3Node(options);
-
+  }
+  var d3n = new D3Node(options)
+  
   var width = 960,
-    height = 500;
+    height = 500
 
   var path = d3.geoPath()
-    .projection(null);
+    .projection(null)
 
   var svg = d3n.createSVG(width, height)
-    .append('path')
+
+  svg.append('path')
     .datum(topojson.mesh(us))
     .attr('class', 'mesh')
-    .attr('d', path);
+    .attr('d', path)
+
+/*
+  svg.selectAll(".start")
+      .data(us.arcs)
+    .enter().append("circle")
+      .attr("class", "start")
+      .attr("transform", function(d) { return "translate(" + transform(d[0]) + ")"; })
+      .attr("r", 2.5);
+
+  function transform(point) {
+    return [
+      point[0] * us.transform.scale[0] + us.transform.translate[0],
+      point[1] * us.transform.scale[1] + us.transform.translate[1]
+    ];
+  }
+*/
 
   const response = {
     statusCode: 200,
     headers: {
      "Content-Type" : "image/svg+xml"
     },
-    body: d3n.svgString(),
+    body: d3n.svgString()
   }
 
   callback(null, response)
